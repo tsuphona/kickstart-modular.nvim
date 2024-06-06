b2043247b30382f36a6716dcbec7b39dc9e9c49b
tsuphona <thomas.suphona@gmail.com>
2024-06-07 00:07:03 +0200

    Add new configs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -40,13 +40,15 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/gruvbox',
 
   require 'kickstart/plugins/todo-comments',
 
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
+
+  require 'kickstart/plugins/lualine',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
--- lua/options.lua
+++ lua/options.lua
@@ -60,3 +60,8 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
